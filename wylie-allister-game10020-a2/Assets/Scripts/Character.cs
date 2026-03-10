743b561044086be9987639f3fffcd558ac58498d
Myota <allister.wylie@mohawkcollege.ca>
2026-03-10 11:35:05 -0400

    Add player movement

--- wylie-allister-game10020-a2/Assets/Scripts/Character.cs
+++ wylie-allister-game10020-a2/Assets/Scripts/Character.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class Character : MonoBehaviour
 {
+    public float moveSpeed = 4f;
+
+    CharacterController controller;
+
+    public InputActionReference moveInput;
+    public InputActionReference hitInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlayerMove();
         
+
+    }
+
+    void PlayerMove()
+    {
+        Vector2 moveDirection = moveInput.action.ReadValue<Vector2>();
+
+        Vector3 move = Vector3.right * moveDirection.x + Vector3.forward * moveDirection.y;
+        Vector3 moveVelo = move * moveSpeed;
+
+        controller.Move(moveVelo * Time.deltaTime);
     }
 }
